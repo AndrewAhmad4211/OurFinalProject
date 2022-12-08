b8865d1f771a5eae04de6355f4bf23196a9cc2a2
Iustin Herciu <118136466+IustinH@users.noreply.github.com>
2022-12-07 20:39:34 -0500

    player class

---
+++ Player.java
@@ -0,0 +1,9 @@
+public class Player{
+    public String first_name;
+    public int height_feet;
+    public int height_inches;
+    public String last_name;
+    public String position;
+    public int weight_pounds;
+
+}
